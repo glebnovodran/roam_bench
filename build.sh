833847d01cc6bd9a625ca65d72201bf07339840c
Glib Novodran <novodran@gmail.com>
2023-04-04 14:51:46 +0200

    Initial commit

---
+++ build.sh
@@ -0,0 +1,367 @@
+#!/bin/sh
+
+PROG_NAME=roam_bench
+BUILD_DATE="$(date)"
+SYS_NAME="`uname -s`"
+BUILD_PATH=$PWD
+
+PROG_DIR=prog
+CORE_DIR=core
+DATA_DIR=data
+
+INC_DIR=inc
+SRC_DIR=src
+TMP_DIR=tmp
+
+
+WEB_TMP_DIR=tmp/web
+
+PROG_PATH=$PROG_DIR/$PROG_NAME
+RUN_PATH=run.sh
+
+if [ "$SYS_NAME" = "FreeBSD" ]; then
+	NO_FMT=""
+fi
+
+FMT_BOLD=${NO_FMT-"\e[1m"}
+FMT_UNDER=${NO_FMT-"\e[4m"}
+FMT_BLACK=${NO_FMT-"\e[30m"}
+FMT_BLACK_BG=${NO_FMT-"\e[40m"}
+FMT_RED=${NO_FMT-"\e[31m"}
+FMT_RED_BG=${NO_FMT-"\e[41m"}
+FMT_GREEN=${NO_FMT-"\e[32m"}
+FMT_GREEN_BG=${NO_FMT-"\e[42m"}
+FMT_YELLOW=${NO_FMT-"\e[33m"}
+FMT_YELLOW_BG=${NO_FMT-"\e[43m"}
+FMT_BLUE=${NO_FMT-"\e[34m"}
+FMT_BLUE_BG=${NO_FMT-"\e[44m"}
+FMT_MAGENTA=${NO_FMT-"\e[35m"}
+FMT_MAGENTA_BG=${NO_FMT-"\e[45m"}
+FMT_CYAN=${NO_FMT-"\e[36m"}
+FMT_CYAN_BG=${NO_FMT-"\e[46m"}
+FMT_WHITE=${NO_FMT-"\e[37m"}
+FMT_WHITE_BG=${NO_FMT-"\e[47m"}
+FMT_GRAY=${NO_FMT-"\e[90m"}
+FMT_GRAY_BG=${NO_FMT-"\e[100m"}
+FMT_B_RED=${NO_FMT-"\e[91m"}
+FMT_B_RED_BG=${NO_FMT-"\e[101m"}
+FMT_B_GREEN=${NO_FMT-"\e[92m"}
+FMT_B_GREEN_BG=${NO_FMT-"\e[102m"}
+FMT_B_YELLOW=${NO_FMT-"\e[93m"}
+FMT_B_YELLOW_BG=${NO_FMT-"\e[103m"}
+FMT_B_BLUE=${NO_FMT-"\e[94m"}
+FMT_B_BLUE_BG=${NO_FMT-"\e[104m"}
+FMT_B_MAGENTA=${NO_FMT-"\e[95m"}
+FMT_B_MAGENTA_BG=${NO_FMT-"\e[105m"}
+FMT_B_CYAN=${NO_FMT-"\e[96m"}
+FMT_B_CYAN_BG=${NO_FMT-"\e[106m"}
+FMT_B_WHITE=${NO_FMT-"\e[97m"}
+FMT_B_WHITE_BG=${NO_FMT-"\e[107m"}
+FMT_OFF=${NO_FMT-"\e[0m"}
+
+WEB_CXX=""
+WEB_BUILD=0
+
+if [ "$#" -gt 0 ]; then
+	case $1 in
+		clean)
+			printf "$FMT_RED$FMT_BOLD""Removing temporary files!""$FMT_OFF\n"
+			for tdir in core inc prog tmp $SHADERS_TGT_DIR; do
+				if [ -d $tdir ]; then
+					printf " * ""$FMT_BOLD""$tdir""$FMT_OFF\n"
+					rm -rf $tdir/*
+					rmdir $tdir
+				fi
+			done
+			if [ -f $RUN_PATH ]; then
+				printf " - ""$FMT_BOLD""$RUN_PATH""$FMT_OFF\n"
+				rm -f $RUN_PATH
+			fi
+			CMK_FILE="CMakeLists.txt"
+			if [ -f $CMK_FILE ]; then
+				rm -f $CMK_FILE
+			fi
+			exit 0
+		;;
+		web)
+			WEB_BUILD=1
+			if [ -n "$EMSDK" ]; then
+				shift
+				WEB_CXX="em++"
+			fi
+		;;
+	esac
+fi
+
+printf "$FMT_MAGENTA$FMT_BOLD""*-----------------------------------------* ""$FMT_OFF\n"
+printf " ""$FMT_BLUE_BG$FMT_B_YELLOW"" .: Building draw-walkthrough project :. ""$FMT_OFF\n"
+printf "$FMT_MAGENTA$FMT_BOLD""*-----------------------------------------* ""$FMT_OFF\n"
+
+if [ $WEB_BUILD -ne 0 ]; then
+	if [ -z "$WEB_CXX" ]; then
+		printf "$FMT_RED$FMT_BOLD""web-build: can't find emscripten""$FMT_OFF\n"
+		exit 1
+	fi
+fi
+
+if [ ! -d $PROG_DIR ]; then
+	mkdir -p $PROG_DIR
+fi
+
+CORE_SRC_URL="https://raw.githubusercontent.com/schaban/crosscore_dev/main/src"
+CORE_SRCS="crosscore.hpp crosscore.cpp demo.hpp demo.cpp draw.hpp oglsys.hpp oglsys.cpp oglsys.inc scene.hpp scene.cpp smprig.hpp smprig.cpp smpchar.hpp smpchar.cpp draw_ogl.cpp main.cpp"
+CORE_OGL_SRCS="gpu_defs.h progs.inc shaders.inc"
+
+CORE_MAC_SRCS=""
+if [ "$SYS_NAME" = "Darwin" ]; then
+	CORE_MAC_SRCS="mac_main.m mac_ifc.h"
+fi
+
+DL_MODE="NONE"
+DL_CMD=""
+
+if [ -x "`command -v curl`" ]; then
+	DL_MODE="CURL"
+	DL_CMD="curl -o"
+elif [ -x "`command -v wget`" ]; then
+	DL_MODE="WGET"
+	DL_CMD="wget -O"
+fi
+
+NEED_CORE=0
+if [ ! -d $CORE_DIR ]; then
+	mkdir -p $CORE_DIR
+	mkdir -p $CORE_DIR/ogl
+	NEED_CORE=1
+else
+	for src in $CORE_SRCS; do
+		if [ $NEED_CORE -ne 1 ]; then
+			if [ ! -f $CORE_DIR/$src ]; then
+				NEED_CORE=1
+			fi
+		fi
+	done
+	if [ ! -d $CORE_DIR/ogl ]; then
+		mkdir -p $CORE_DIR/ogl
+		NEED_CORE=1
+	fi
+	if [ $NEED_CORE -ne 1 ]; then
+		for src in $CORE_OGL_SRCS; do
+			if [ ! -f $CORE_DIR/ogl/$src ]; then
+				NEED_CORE=1
+			fi
+		done
+	fi
+	if [ $NEED_CORE -ne 1 ]; then
+		if [ -n "$CORE_MAC_SRCS" ]; then
+			for src in $CORE_MAC_SRCS; do
+				if [ ! -f $CORE_DIR/mac/$src ]; then
+					NEED_CORE=1
+				fi
+			done
+		fi
+	fi
+fi
+
+if [ $NEED_CORE -ne 0 ]; then
+	printf "$FMT_B_RED""-> Downloading crosscore sources...""$FMT_OFF\n"
+	for src in $CORE_SRCS; do
+		if [ ! -f $CORE_DIR/$src ]; then
+			printf "$FMT_B_BLUE""     $src""$FMT_OFF\n"
+			$DL_CMD $CORE_DIR/$src $CORE_SRC_URL/$src
+		fi
+	done
+	for src in $CORE_OGL_SRCS; do
+		if [ ! -f $CORE_DIR/ogl/$src ]; then
+			printf "$FMT_CYAN""     $src""$FMT_OFF\n"
+			$DL_CMD $CORE_DIR/ogl/$src $CORE_SRC_URL/ogl/$src
+		fi
+	done
+	if [ -n "$CORE_MAC_SRCS" ]; then
+		mkdir -p $CORE_DIR/mac
+		for src in $CORE_MAC_SRCS; do
+			if [ ! -f $CORE_DIR/mac/$src ]; then
+				printf "$FMT_CYAN""     $src""$FMT_OFF\n"
+				$DL_CMD $CORE_DIR/mac/$src $CORE_SRC_URL/mac/$src
+			fi
+		done
+	fi
+fi
+
+NEED_OGL_INC=0
+if [ $WEB_BUILD -eq 0 ]; then
+	case $SYS_NAME in
+		Linux | SunOS)
+			NEED_OGL_INC=1
+		;;
+	esac
+fi
+
+if [ $NEED_OGL_INC -ne 0 ]; then
+	KHR_REG_URL=https://registry.khronos.org
+	INC_OGL=$INC_DIR/GL
+	if [ ! -d $INC_OGL ]; then
+		mkdir -p $INC_OGL
+	fi
+	OGL_INC_DL=0
+	for kh in glext.h glcorearb.h; do
+		if [ ! -f $INC_OGL/$kh ]; then
+			if [ $OGL_INC_DL -ne 1 ]; then
+				printf "$FMT_OFF""-> Downloading OpenGL headers...\n"
+				OGL_INC_DL=1
+			fi
+			printf "$FMT_B_GREEN""     $kh""$FMT_OFF\n"
+			$DL_CMD $INC_OGL/$kh $KHR_REG_URL/OpenGL/api/GL/$kh
+		fi
+	done
+	INC_KHR=$INC_DIR/KHR
+	if [ ! -d $INC_KHR ]; then
+		mkdir -p $INC_KHR
+	fi
+	for kh in khrplatform.h; do
+		if [ ! -f $INC_KHR/$kh ]; then
+			if [ $OGL_INC_DL -ne 1 ]; then
+				printf "$FMT_OFF""-> Downloading OpenGL headers...\n"
+				OGL_INC_DL=1
+			fi
+			printf "$FMT_B_GREEN""     $kh""$FMT_OFF\n"
+			$DL_CMD $INC_KHR/$kh $KHR_REG_URL/EGL/api/KHR/$kh
+		fi
+	done
+fi
+
+INCS="-I $CORE_DIR -I $INC_DIR "
+SRCS="`ls $SRC_DIR/*.cpp` `ls $CORE_DIR/*.cpp`"
+
+DEFS="-DX11"
+LIBS="-lX11"
+
+DEF_CXX="g++"
+case $SYS_NAME in
+	OpenBSD)
+		INCS="$INCS -I/usr/X11R6/include"
+		LIBS="$LIBS -L/usr/X11R6/lib"
+		DEF_CXX="clang++"
+	;;
+	FreeBSD)
+		INCS="$INCS -I/usr/local/include"
+		LIBS="$LIBS -lpthread -L/usr/local/lib"
+		DEF_CXX="clang++"
+	;;
+	Darwin)
+		DEFS=""
+		LIBS="-framework Foundation -framework Cocoa -framework OpenGL -Xlinker -w"
+		DEF_CXX="clang++"
+	;;
+	Linux)
+		LIBS="$LIBS -ldl -lpthread"
+	;;
+esac
+CXX=${CXX:-$DEF_CXX}
+
+if [ "$#" -gt 0 ]; then
+	case $1 in
+		srcs)
+			shift
+			SRCS_OUT=
+			printf "$FMT_GREEN""-> Dumping sources list...""$FMT_OFF\n"
+			if [ "$#" -gt 0 ]; then
+				SRCS_OUT=$1
+				shift
+			fi
+			SCRCS_FLG=0
+			if [ "$#" -gt 0 ]; then
+				if [ "$1" = "append" ]; then
+					SCRCS_FLG=1
+				fi
+				shift
+			fi
+			for src in $SRCS; do
+				if [ -n "$SRCS_OUT" ]; then
+					if [ $SCRCS_FLG -ne 0 ]; then
+						echo $src >> $SRCS_OUT
+					else
+						echo $src > $SRCS_OUT
+					fi
+				else
+					echo $src
+				fi
+				SCRCS_FLG=1
+			done
+			printf "Done.\n"
+			exit 0
+		;;
+	esac
+fi
+
+
+if [ $WEB_BUILD -ne 0 ]; then
+	PROG_HTML=$PROG_PATH.html
+	if [ ! -d "$WEB_TMP_DIR" ]; then
+		mkdir -p $WEB_TMP_DIR
+	fi
+	XROM_PATH="$BUILD_PATH/$WEB_TMP_DIR/xrom.cpp"
+	MKROM_SRC="$WEB_TMP_DIR/mkrom.cpp"
+	if [ ! -f "$MKROM_SRC" ]; then
+		printf "$FMT_B_BLUE""Downloading mkrom...""$FMT_OFF\n"
+		$DL_CMD $MKROM_SRC $CORE_SRC_URL/etc/rom/mkrom.cpp
+	fi
+	MKROM_EXE="$BUILD_PATH/$WEB_TMP_DIR/mkrom"
+	if [ ! -f "$MKROM_EXE" ]; then
+		printf "$FMT_B_BLUE""Compiling mkrom...""$FMT_OFF\n"
+		MKROM_CXX_FLGS="-O2 -flto -pthread -I $CORE_DIR"
+		MKROM_CXX_SRCS="$CORE_DIR/crosscore.cpp $MKROM_SRC"
+		$CXX $MKROM_CXX_FLGS $MKROM_CXX_SRCS -o $MKROM_EXE
+	fi
+	printf "Making ROM file:\n"
+	cd $DATA_DIR
+	ls -1d */* | $MKROM_EXE -nobin:1 -txt:$XROM_PATH
+	cd $BUILD_PATH
+	WEB_OPTS="-std=c++11 -O3"
+	WEB_OPTS="$WEB_OPTS -s WASM=0 -s SINGLE_FILE"
+	WEB_OPTS="$WEB_OPTS -s SINGLE_FILE -s USE_SDL=2 -s ASSERTIONS=1"
+	WEB_OPTS="$WEB_OPTS -s INITIAL_MEMORY=50MB -s ALLOW_MEMORY_GROWTH=1"
+	WEB_OPTS="$WEB_OPTS -DXD_THREADFUNCS_ENABLED=0 -DXD_FILEFUNCS_ENABLED=0"
+	WEB_OPTS="$WEB_OPTS -DOGLSYS_WEB -DUSE_XROM_ARCHIVE"
+	printf "Compiling \"$FMT_BOLD$FMT_B_MAGENTA$PROG_HTML$FMT_OFF\" with $FMT_BOLD$WEB_CXX$FMT_OFF.\n"
+	$WEB_CXX $WEB_OPTS $INCS $SRCS $XROM_PATH --pre-js src/web/opt.js --shell-file src/web/shell.html -o $PROG_HTML -s EXPORTED_RUNTIME_METHODS='["ccall","cwrap"]' -s EXPORTED_FUNCTIONS='["_main","_wi_set_key_state"]' $*
+	if [ -f "$PROG_HTML" ]; then
+		sed -i 's/antialias:!1/antialias:1/g' $PROG_HTML
+		DATE_EXPR="s/Build date:.../Build date: $BUILD_DATE | JS\/ROM ver./g"
+		sed -i "$DATE_EXPR" $PROG_HTML
+		printf "web-build: ""$FMT_B_GREEN""Success""$FMT_OFF""$FMT_BOLD""!!""$FMT_OFF\n"
+	else
+		printf "web-build: ""$FMT_B_RED""Failure""$FMT_OFF...\n"
+	fi
+	exit
+fi
+
+printf "Compiling \"$FMT_BOLD$FMT_B_MAGENTA$PROG_PATH$FMT_OFF\" with $FMT_BOLD$CXX$FMT_OFF.\n"
+rm -f $PROG_PATH
+rm -f $RUN_PATH
+if [ "$SYS_NAME" = "Darwin" ]; then
+	if [ ! -d $TMP_DIR ]; then
+		mkdir -p $TMP_DIR
+	fi
+	MAC_MAIN_SRC=$CORE_DIR/mac/mac_main.m
+	MAC_MAIN_OBJ=$TMP_DIR/mac_main.o
+	rm -f $MAC_MAIN_OBJ
+	clang -Wno-deprecated-declarations $MAC_MAIN_SRC -Og -c -o $MAC_MAIN_OBJ
+	if [ ! -f $MAC_MAIN_OBJ ]; then
+		exit 1
+	fi
+	SRCS="$SRCS $MAC_MAIN_OBJ"
+	INCS="$INCS -I $CORE_DIR/mac"
+fi
+
+$CXX -std=c++11 -ggdb $DEFS $INCS $SRCS -o $PROG_PATH $LIBS $*
+
+if [ -f "$PROG_PATH" ]; then
+	printf "#!/bin/sh\n\n" > $RUN_PATH
+	printf "./$PROG_PATH -nwrk:0 \$*\n" >> $RUN_PATH
+	chmod +x $RUN_PATH
+	printf "$FMT_B_GREEN""Success""$FMT_OFF""$FMT_BOLD""!!""$FMT_OFF\n"
+else
+	printf "$FMT_B_RED""Failure""$FMT_OFF...\n"
+fi
+
---
+++ src/demo_default.cpp
@@ -0,0 +1,373 @@
+#include "crosscore.hpp"
+#include "scene.hpp"
+#include "demo.hpp"
+#include "smprig.hpp"
+#include "smpchar.hpp"
+
+DEMO_PROG_BEGIN
+
+static cxStopWatch s_execStopWatch;
+static float s_medianExecMillis = -1.0f;
+static int s_exerep = 1;
+
+enum class RoamProgKind {
+	NATIVE,
+	PINT,
+	PLOP
+};
+
+static RoamProgKind s_roamProgKind = RoamProgKind::NATIVE;
+
+static struct STAGE {
+	Pkg* pPkg;
+	sxCollisionData* pCol;
+} s_stage = {};
+
+static void add_stg_obj(sxModelData* pMdl, void* pWkData) {
+	ScnObj* pObj = Scene::add_obj(pMdl);
+	if (pObj) {
+		pObj->set_base_color_scl(1.0f);
+	}
+}
+
+static void init_stage() {
+	Pkg* pPkg = Scene::load_pkg("slope_room");
+	s_stage.pPkg = pPkg;
+	s_stage.pCol = nullptr;
+	if (pPkg) {
+		Scene::for_all_pkg_models(pPkg, add_stg_obj, &s_stage);
+		SmpCharSys::set_collision(pPkg->find_collision("col"));
+	}
+}
+
+static void char_roam_ctrl(SmpChar* pChar) {
+	if (!pChar) return;
+	double objTouchDT = pChar->get_obj_touch_duration_secs();
+	double wallTouchDT = pChar->get_wall_touch_duration_secs();
+	switch (pChar->mAction) {
+		case SmpChar::ACT_STAND:
+			if (pChar->check_act_time_out()) {
+				if ((Scene::glb_rng_next() & 0x3F) < 0xF) {
+					pChar->change_act(SmpChar::ACT_RUN, 2.0f);
+				} else {
+					pChar->change_act(SmpChar::ACT_WALK, 5.0f);
+					pChar->reset_wall_touch();
+				}
+			}
+			break;
+		case SmpChar::ACT_WALK:
+			if (pChar->check_act_time_out() || objTouchDT > 0.2 || wallTouchDT > 0.25) {
+				if (objTouchDT > 0 && ((Scene::glb_rng_next() & 0x3F) < 0x1F)) {
+					pChar->change_act(SmpChar::ACT_RETREAT, 0.5f);
+				} else {
+					float t = nxCalc::fit(Scene::glb_rng_f01(), 0.0f, 1.0f, 0.5f, 2.0f);
+					if (Scene::glb_rng_next() & 0x1) {
+						pChar->change_act(SmpChar::ACT_TURN_L, t);
+					} else {
+						pChar->change_act(SmpChar::ACT_TURN_R, t);
+					}
+				}
+			}
+			break;
+		case SmpChar::ACT_RUN:
+			if (pChar->check_act_time_out() || wallTouchDT > 0.1 || objTouchDT > 0.1) {
+				pChar->change_act(SmpChar::ACT_STAND, 2.0f);
+			}
+			break;
+		case SmpChar::ACT_RETREAT:
+			if (pChar->check_act_time_out() || wallTouchDT > 0.1) {
+				pChar->change_act(SmpChar::ACT_STAND, 2.0f);
+			}
+			break;
+		case SmpChar::ACT_TURN_L:
+		case SmpChar::ACT_TURN_R:
+			if (pChar->check_act_time_out()) {
+				pChar->change_act(SmpChar::ACT_STAND, 1.0f);
+			}
+			break;
+	}
+}
+
+static void init_chars() {
+	SmpChar::Descr descr;
+	descr.reset();
+	bool disableSl = nxApp::get_bool_opt("scl_off", false);
+
+	float x = -5.57f;
+	for (int i = 0; i < 10; ++i) {
+		descr.variation = i;
+		if (disableSl) {
+			descr.scale = 1.0f;
+		} else {
+			if (!(i & 1)) {
+				descr.scale = 0.95f;
+			} else {
+				descr.scale = 1.0f;
+			}
+		}
+		ScnObj* pObj = SmpCharSys::add_f(descr, char_roam_ctrl);
+		pObj->set_world_quat_pos(nxQuat::from_degrees(0.0f, 0.0f, 0.0f), cxVec(x, 0.0f, 0.0f));
+		x += 0.7f;
+	}
+
+	cxVec pos(-4.6f, 0.0f, 7.0f);
+	cxQuat q = nxQuat::from_degrees(0.0f, 110.0f, 0.0f);
+	cxVec add = q.apply(cxVec(0.7f, 0.0f, 0.0f));
+	for (int i = 0; i < 10; ++i) {
+		descr.variation = i;
+		if (disableSl) {
+			descr.scale = 1.0f;
+		} else {
+			if (!(i & 1)) {
+				descr.scale = 1.0f;
+			} else {
+				descr.scale = 1.04f;
+			}
+		}
+		ScnObj* pObj = SmpCharSys::add_m(descr, char_roam_ctrl);
+		if (pObj) {
+			pObj->set_world_quat_pos(q, pos);
+		}
+		pos += add;
+	}
+
+	int mode = nxApp::get_int_opt("mode", 1);
+	if (mode == 1) {
+		cxVec pos(-8.0f, 0.0f, -2.0f);
+		cxVec add(0.75f, 0.0f, 0.0f);
+		cxQuat q = nxQuat::identity();
+		for (int i = 0; i < 20; ++i) {
+			ScnObj* pObj = nullptr;
+			descr.scale = 1.0f;
+			descr.variation = 9 - (i >> 1);
+			if (i & 1) {
+				pObj = SmpCharSys::add_f(descr, char_roam_ctrl);
+			} else {
+				pObj = SmpCharSys::add_m(descr, char_roam_ctrl);
+			}
+			if (pObj) {
+				pObj->set_world_quat_pos(q, pos);
+			}
+			pos += add;
+			if (i == 9) {
+				pos.x = -8.0f;
+				pos.z += -1.25f;
+			}
+		}
+	}
+}
+
+
+static void init() {
+	s_roamProgKind = RoamProgKind::NATIVE;
+	const char* pRoamProgOpt = nxApp::get_opt("roamprog");
+	if (pRoamProgOpt) {
+		if (nxCore::str_eq(pRoamProgOpt, "pint")) {
+			s_roamProgKind = RoamProgKind::PINT;
+		} else if (nxCore::str_eq(pRoamProgOpt, "plop")) {
+			s_roamProgKind = RoamProgKind::PLOP;
+		}
+	}
+
+	//Scene::alloc_global_heap(1024 * 1024 * 2);
+	Scene::alloc_local_heaps(1024 * 1024 * 2);
+	SmpCharSys::init();
+	init_chars();
+	init_stage();
+	s_execStopWatch.alloc(120);
+	Scene::enable_split_move(nxApp::get_bool_opt("split_move", false));
+	nxCore::dbg_msg("Scene::split_move: %s\n", Scene::is_split_move_enabled() ? "Yes" : "No");
+	s_exerep = nxCalc::clamp(nxApp::get_int_opt("exerep", 1), 1, 100);
+	nxCore::dbg_msg("exerep: %d\n", s_exerep);
+	nxCore::dbg_msg("Scene::speed: %.2f\n", Scene::speed());
+	int ncpus = nxSys::num_active_cpus();
+	nxCore::dbg_msg("num active CPUs: %d\n", ncpus);
+
+	nxCore::dbg_msg("roam prog kind: ");
+	if (s_roamProgKind == RoamProgKind::PINT) {
+		nxCore::dbg_msg("PINT");
+	} else if (s_roamProgKind == RoamProgKind::PLOP) {
+		nxCore::dbg_msg("PLOP");
+	} else {
+		nxCore::dbg_msg("NATIVE");
+	}
+	nxCore::dbg_msg("\n");
+}
+
+static struct ViewWk {
+	float t;
+	float dt;
+
+	void update() {
+		t += dt * SmpCharSys::get_motion_speed() * 2.0f;
+		if (t > 1.0f) {
+			t = 1.0f;
+			dt = -dt;
+		} else if (t <= 0.0f) {
+			t = 0.0f;
+			dt = -dt;
+		}
+	}
+} s_view;
+
+static void view_exec() {
+	if (s_view.dt == 0.0f) {
+		s_view.t = 0.0f;
+		s_view.dt = 0.0025f;
+	}
+	float tz = nxCalc::ease(s_view.t);
+	cxVec tgt = cxVec(0.0f, 1.0f, nxCalc::fit(tz, 0.0f, 1.0f, 0.0f, 6.0f));
+	cxVec pos = cxVec(3.9f, 1.9f, 3.5f);
+	Scene::set_view(pos, tgt);
+	s_view.update();
+}
+
+static void set_scene_ctx() {
+	Scene::set_shadow_dir_degrees(70.0f, 140.0f);
+	Scene::set_shadow_proj_params(20.0f, 10.0f, 50.0f);
+	Scene::set_shadow_fade(28.0f, 35.0f);
+	Scene::set_shadow_uniform(false);
+
+	Scene::set_spec_dir_to_shadow();
+	Scene::set_spec_shadowing(0.75f);
+
+	Scene::set_hemi_upper(2.5f, 2.46f, 2.62f);
+	Scene::set_hemi_lower(0.32f, 0.28f, 0.26f);
+	Scene::set_hemi_up(Scene::get_shadow_dir().neg_val());
+	Scene::set_hemi_exp(2.5f);
+	Scene::set_hemi_gain(0.7f);
+
+	Scene::set_fog_rgb(0.748f, 0.79f, 0.87f);
+	Scene::set_fog_density(1.0f);
+	Scene::set_fog_range(1.0f, 200.0f);
+	Scene::set_fog_curve(0.1f, 0.1f);
+
+	Scene::set_exposure_rgb(0.75f, 0.85f, 0.5f);
+
+	Scene::set_linear_white_rgb(0.65f, 0.65f, 0.55f);
+	Scene::set_linear_gain(1.32f);
+	Scene::set_linear_bias(-0.025f);
+}
+
+static void draw_2d() {
+	char str[512];
+	const char* fpsStr = "FPS: ";
+	const char* exeStr = "EXE: ";
+	float refSizeX = 550.0f;
+	float refSizeY = 350.0f;
+	Scene::set_ref_scr_size(refSizeX, refSizeY);
+	if (Scene::get_view_mode_width() < Scene::get_view_mode_height()) {
+		Scene::set_ref_scr_size(refSizeY, refSizeX);
+	}
+
+	float sx = 10.0f;
+	float sy = Scene::get_ref_scr_height() - 20.0f;
+
+	xt_float2 bpos[4];
+	xt_float2 btex[4];
+	btex[0].set(0.0f, 0.0f);
+	btex[1].set(1.0f, 0.0f);
+	btex[2].set(1.0f, 1.0f);
+	btex[3].set(0.0f, 1.0f);
+	float bx = 4.0f;
+	float by = 4.0f;
+	float bw = 280.0f;
+	float bh = 12.0f;
+	cxColor bclr(0.0f, 0.0f, 0.0f, 0.75f);
+	bpos[0].set(sx - bx, sy - by);
+	bpos[1].set(sx + bw + bx, sy - by);
+	bpos[2].set(sx + bw + bx, sy + bh + by);
+	bpos[3].set(sx - bx, sy + bh + by);
+	Scene::quad(bpos, btex, bclr);
+
+	float fps = SmpCharSys::get_fps();
+	if (fps <= 0.0f) {
+		XD_SPRINTF(XD_SPRINTF_BUF(str, sizeof(str)), "%s--", fpsStr);
+	} else {
+		XD_SPRINTF(XD_SPRINTF_BUF(str, sizeof(str)), "%s%.2f", fpsStr, fps);
+	}
+	Scene::print(sx, sy, cxColor(0.1f, 0.75f, 0.1f, 1.0f), str);
+	sx += 120.0f;
+
+	float exe = s_medianExecMillis;
+	if (exe <= 0.0f) {
+		XD_SPRINTF(XD_SPRINTF_BUF(str, sizeof(str)), "%s--", exeStr);
+	} else {
+		XD_SPRINTF(XD_SPRINTF_BUF(str, sizeof(str)), "%s%.4f millis", exeStr, exe);
+	}
+	Scene::print(sx, sy, cxColor(0.5f, 0.4f, 0.1f, 1.0f), str);
+}
+
+static void profile_start() {
+	s_execStopWatch.begin();
+}
+
+static void profile_end() {
+	if (s_execStopWatch.end()) {
+		double us = s_execStopWatch.median();
+		s_execStopWatch.reset();
+		double millis = us / 1000.0;
+		s_medianExecMillis = float(millis);
+		Scene::thermal_info();
+		Scene::battery_info();
+	}
+}
+
+static bool chr_exec_init_pint_func(ScnObj* pObj, void* pWkMem) {
+	SmpChar* pChr = SmpCharSys::char_from_obj(pObj);
+	if (pChr) {
+		//
+	}
+	return true;
+}
+
+static void chr_exec_init() {
+	if (s_roamProgKind == RoamProgKind::PINT) {
+		Scene::for_each_obj(chr_exec_init_pint_func, nullptr);
+	}
+}
+
+static bool chr_exec_reset_pint_func(ScnObj* pObj, void* pWkMem) {
+	SmpChar* pChr = SmpCharSys::char_from_obj(pObj);
+	if (pChr) {
+		//
+	}
+	return true;
+}
+
+static void chr_exec_reset() {
+	if (s_roamProgKind == RoamProgKind::PINT) {
+		Scene::for_each_obj(chr_exec_reset_pint_func, nullptr);
+	}
+}
+
+static void scn_exec() {
+	for (int i = 0; i < s_exerep; ++i) {
+		chr_exec_init();
+		Scene::exec();
+		chr_exec_reset();
+	}
+}
+
+static void loop(void* pLoopCtx) {
+	SmpCharSys::start_frame();
+	set_scene_ctx();
+	profile_start();
+	scn_exec();
+	view_exec();
+	Scene::visibility();
+	profile_end();
+	Scene::frame_begin(cxColor(0.25f));
+	Scene::draw();
+	draw_2d();
+	Scene::frame_end();
+}
+
+static void reset() {
+	SmpCharSys::reset();
+	s_execStopWatch.free();
+}
+
+DEMO_REGISTER(default);
+
+DEMO_PROG_END
